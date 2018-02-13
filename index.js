7a47971ec466740daed4643724faf0eccc99eb00
Snugglez <32309975+Snugglez@users.noreply.github.com>
2018-02-12 22:31:00 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const config = require('./config.json');
+const Command = require('command');
+module.exports = function dropnotice(dispatch) {
+const command = Command(dispatch)
+let enabled = config.toggle;
+
+command.add('dropnotice', () => {
+enabled = !enabled
+command.message(`Drop-Notice is now ${enabled ? 'enabled' : 'disabled'}.`)})
+
+dispatch.hook('S_SPAWN_DROPITEM', 1, (event) => {
+if(!enabled) return;
+if(event.item === 369)
+command.message('Diamond drop')
+else if(event.item === 98512)
+command.message('Pansophic Ash drop')
+else if(event.item === 98513)
+command.message('Elemental Essence')
+else if(event.item === 98281)
+command.message('Superior Etching Box drop')
+else if(event.item === 98521)
+command.message('Otherworldly Shard drop')
+})
+}
+
